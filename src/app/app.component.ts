e7754533b5c6d7c49d0ac6fc24ef231b3487e881
Robert <90275291+RobertLeeuwerink@users.noreply.github.com>
2022-07-08 09:41:01 -0700

    add: async validator and give different validator options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
-import { FormGroup, FormControl, ValidatorFn, AbstractControl, ValidationErrors } from "@angular/forms";
-import { BehaviorSubject } from "rxjs";
-import { ApiMsgValidator } from "./ApiMsg.validator";
+import { FormGroup, FormControl, ValidatorFn, AbstractControl, ValidationErrors, AsyncValidatorFn } from "@angular/forms";
+import { BehaviorSubject, map, Observable, of, takeWhile } from "rxjs";
+import { LogService } from "./log.service";
 
 
 @Component({
@@ -15,14 +15,14 @@ import { ApiMsgValidator } from "./ApiMsg.validator";
   });
   editMode = new BehaviorSubject<boolean>(true);
 
-  validations: {[id: string]: ValidatorFn} = {};
+  validations: {[id: string]: ValidatorFn | AsyncValidatorFn} = {};
 
   get userName() {
     return this.form.get('userName');
   }
 
-  constructor(private apiMsgValidator: ApiMsgValidator) {
-    this.form.valueChanges.subscribe(() => console.log(this.form));
+  constructor(private logService: LogService) {
+    // this.form.valueChanges.subscribe(() => console.log(this.form));
   }
 
   updateUser() {
@@ -42,14 +42,13 @@ import { ApiMsgValidator } from "./ApiMsg.validator";
     const message = 'Luke I am your father, come join me on the backend team';
     this.validations = {
       ...this.validations,
-      [id]: apiMsgValidator(message, id),
+      [id]: apiMsgValidatorWithService(message, id, this.logService),
     };
     this.form.controls['userName'].setErrors({ 
-      invalid: true,
-      backend: true,
-      msg_id: id,
+      validatorFnId: id,
       message
     });
+    this.logService.addError(id, message);
     this.form.controls['userName'].markAsUntouched();
     this.form.controls['userName'].addValidators(this.validations[id]);
     this.form.updateValueAndValidity();
@@ -58,15 +57,17 @@ import { ApiMsgValidator } from "./ApiMsg.validator";
   addAsyncError() {
     const id = '17';
     const message = 'Luke I am your father, come join me on the backend team';
-    this.apiMsgValidator.addApiMsg(id, message);
+    this.validations = {
+      ...this.validations,
+      [id]: apiMsgValidatorAsyncFn(message, id, this.logService),
+    };
+    this.logService.addError(id, message);
     this.form.controls['userName'].setErrors({ 
-      invalid: true,
-      backend: true,
-      msg_id: id,
+      validatorFnId: id,
       message,
-      async: true 
+      async: true
     });
-    this.form.controls['userName'].addAsyncValidators(this.apiMsgValidator.validate);
+    this.form.get('userName')?.addAsyncValidators(this.validations[id] as AsyncValidatorFn);
     this.form.updateValueAndValidity();
   }
 
@@ -76,31 +77,63 @@ import { ApiMsgValidator } from "./ApiMsg.validator";
   }
 
   removeValidator(id: string) {
-    this.form.controls['userName'].removeValidators(this.validations[id]);
-    this.form.controls['userName'].setErrors({ 
-      invalid: null,
-      backend: null  });
+    this.form.get('userName')?.removeValidators(this.validations[id]);
+    this.form.get('userName')?.setErrors(null);
+    this.logService.removeError(id);
     this.form.updateValueAndValidity();
   }
-  removeAsyncValidator(id: string) {
-    this.apiMsgValidator.removeApiMsg(id);
+
+  removeValidatorAsync(id: string) {
+    this.form.get('userName')?.removeAsyncValidators(this.validations[id] as AsyncValidatorFn);
+    this.form.get('userName')?.setErrors(null);
+    this.logService.removeError(id);
+    this.form.updateValueAndValidity();
   }
 }
 
-export function apiMsgValidator(message: string, id: string): ValidatorFn {
+export function apiMsgValidatorDif(message: string, validatorFnId: string, original_value: string): ValidatorFn {
+  // this keeps the error around but will be marked valid aslong as the value is different from the one at the point this validator is created. so if someone removes the changes they made the error returns
   return (control: AbstractControl): ValidationErrors | null => {
-    const bakendMsg = true;
-    console.log(control)
-    return !control.touched ? { 
-      invalid: true,
-      backend: true,
-      msg_id: id,
-      message
-     } : { 
-      invalid: false,
-      backend: false,
-      msg_id: id,
-      message
+    return original_value !== control.getRawValue() ? { 
+      message,
+      validatorFnId
+     } : null;
+  };
+}
+
+export function apiMsgValidatorOnControlTouched(message: string, validatorFnId: string): ValidatorFn {
+  // requires the control to be set to untouched so it doesn't need to get removed but if marked as untouched again the error will return
+  return (control: AbstractControl): ValidationErrors | null => {
+    return control.untouched ? { 
+      message,
+      validatorFnId
+     } : null;
+  };
+}
+
+export function apiMsgValidatorDeleteValidator(message: string, validatorFnId: string): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    // control.removeValidators( instance of ValidatorFn ) you have to remove error from the control to make the form valid. So you bubble back to originating ts file where you should have the ValidatorFn instance
+    return { 
+      message,
+      validatorFnId
      };
   };
+}
+
+
+export function apiMsgValidatorWithService(message: string, validatorFnId: string, logService: LogService): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => { 
+    return logService.findError(validatorFnId) ? 
+      { 
+        message,
+        validatorFnId
+       } :  null; 
+  }
+}
+
+export function apiMsgValidatorAsyncFn(message: string, validatorFnId: string, logService: LogService): AsyncValidatorFn {
+  return (control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => 
+    // logService.findError(validatorFnId) ? of({ message, validatorFnId, async: true }) : of(null);
+    new Promise((resolve, reject) => resolve(logService.findError(validatorFnId) ? { message, validatorFnId, async: true } : null));  
 }
